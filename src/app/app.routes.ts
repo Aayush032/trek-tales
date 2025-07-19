3eb249101067b703c65bfc18d846883b89bfb328
Aayush Maharjan <aayushmaharjan032@gmail.com>
2025-07-19 21:58:42 +0545

    Added: added landing section.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+        {
+        path:'',
+        redirectTo:"homePage",
+        pathMatch:'full',
+        title:'Home Page'
+    },
+    {
+        path:'homePage',
+        component: HomePageComponent,
+        title:'Home Page'
+    },
+];
